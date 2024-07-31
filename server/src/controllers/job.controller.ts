f8b11930134f9d4da875ed6d2d748a64553e874f
Prajwal-PL <prajwalpl096@gmail.com>
2024-07-31 19:57:01 +0530

    removed salary

--- server/src/controllers/job.controller.ts
+++ server/src/controllers/job.controller.ts
@@ -42,7 +42,6 @@ export const addJob: RequestHandler = async (req, res) => {
         title,
         description,
         location,
-        salary,
         User: {
           connect: {
             id: userId,
